d8b1fbd658446f764040b3e85be66cb4fa2ebfcb
Kristen <35566182+KristenJes@users.noreply.github.com>
2019-03-07 11:57:57 +0100

    editing / adding form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -38,7 +39,7 @@ class BlogController extends AbstractController
      * @Route("/blog/new", name="blog_create")
      * @Route("/blog/{id}/edit", name="blog_edit")
      */
-    public function form(Article $article = numfmt_get_locale, Request $request, ObjectManager $manager){
+    public function form(Article $article = null, Request $request, ObjectManager $manager){
         if(!$article){
             $article = new Article();
         }
@@ -52,16 +53,19 @@ class BlogController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            $article-setCreatedAt(new \DateTime());
+            if(!$article->getId()){
+                $article->setCreatedAt(new \DateTime());
+            }
 
             $manager->persist($article);
-            $manager->fush();
+            $manager->flush();
 
             return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
         }
 
         return $this->render("blog/create.html.twig", [
-            'formArticle' => $form->createView()
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
         ]);
 
     }
